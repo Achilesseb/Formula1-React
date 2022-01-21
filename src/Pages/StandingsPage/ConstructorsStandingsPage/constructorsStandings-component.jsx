f80fd1e0770d4c215ffa00d558733d661eca3f99
Achilesseb <prisacariuvictor@gmail.com>
2022-01-21 19:48:14 +0200

    Refactored code on DriverStandings

--- src/Pages/StandingsPage/ConstructorsStandingsPage/constructorsStandings-component.jsx
+++ src/Pages/StandingsPage/ConstructorsStandingsPage/constructorsStandings-component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./constructorsStandings.styles.scss";
 import "../../../root.scss";
 import logo from "../../../navigation/navigation-links/logo.png";
+import { useSelector } from "react-redux";
 
 const ConstructorStandingsPage = () => {
   const constructorsStandingsData = useSelector(
@@ -10,20 +11,21 @@ const ConstructorStandingsPage = () => {
   const ConstructorStandingBody = () => {
     const ConstructorsStandingsRow = () =>
       constructorsStandingsData.map((standing) => {
+        let data = [];
         const { position, points } = standing;
         const { name, constructorId } = standing.Constructor;
+        data.push([position, name, points]);
         const trStyle = {
           backgroundColor: `var(--color-${constructorId})`,
         };
         return (
           <tr style={trStyle} className="constructors-standigs-row-item">
-            <th className="constructors-standings-cell position">{position}</th>
-            <th className="constructors-standings-cell name">{name}</th>
-            <th className="constructors-standings-cell points">{points} pts</th>
+            {data[0].map((data) => (
+              <th className="constructors-standings-cell">{data}</th>
+            ))}
           </tr>
         );
       });
-
     return (
       <tbody className="constructors-standings-page-tablebody">
         <ConstructorsStandingsRow />
--- src/Pages/StandingsPage/DriversStandingsPage/driversStandingsPage-component.jsx
+++ src/Pages/StandingsPage/DriversStandingsPage/driversStandingsPage-component.jsx
@@ -1,76 +1,53 @@
 import React from "react";
 import "./driversStandingsPage.styles.scss";
 import logo from "../../../navigation/navigation-links/logo.png";
+import { useSelector } from "react-redux";
 
-class DriversStandingsPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      standings: [],
-      date: Date(),
-    };
-  }
-  async componentDidMount() {
-    await fetch("http://ergast.com/api/f1/current/driverStandings.json")
-      .then((res) => res.json())
-      .then((result) => {
-        const standingsRaw =
-          result.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-
-        this.setState({ standings: standingsRaw }, () =>
-          console.log(this.state)
-        );
-      });
-  }
-  render() {
-    const StandingTable = () =>
-      this.state.standings.map((standings) => {
-        let data = [];
-        const { position, points, wins } = standings;
-        const {
-          dateOfBirth,
-          givenName,
-          familyName,
-          permanentNumber,
-          nationality,
-        } = standings.Driver;
-        const { name } = standings.Constructors[0];
-        return (
-          <tr className="driver-standigs-table-row">
-            <td className="driver-standigs-table-cell">{position}</td>
-            <td className="driver-standigs-table-cell">{`${
-              givenName + " " + familyName
-            }`}</td>
-            <td className="driver-standigs-table-cell">{points}</td>
-            <td className="driver-standigs-table-cell">{wins}</td>
-            <td className="driver-standigs-table-cell">{permanentNumber}</td>
-            <td className="driver-standigs-table-cell">{nationality}</td>
-            <td className="driver-standigs-table-cell">{name}</td>
+const DriversStandingsPage = () => {
+  const driversStandingData = useSelector((data) => data[0].DriverStandings);
+  console.log(driversStandingData);
+  const StandingTable = () =>
+    driversStandingData.map((standings) => {
+      let data = [];
+      const { position, points, wins } = standings;
+      const { givenName, familyName } = standings.Driver;
+      const driverName = `${givenName + " " + familyName}`;
+      const { name: teamName, constructorId } = standings.Constructors[0];
+      data.push([position, driverName, points, wins]);
+      return (
+        <tr className="driver-standigs-table-row">
+          {data[0].map((data) => (
+            <td className="driver-standigs-table-cell">{data}</td>
+          ))}
+          <td
+            style={{ color: `var(--color-${constructorId})`, opacity: 0.9 }}
+            className="driver-standigs-table-cell"
+          >
+            {teamName}
+          </td>
+        </tr>
+      );
+    });
+  return (
+    <div className="Standings">
+      <div className="Standings-label">2021 DRIVERS Standings</div>
+      <table className="driver-standigs-table">
+        <tbody className="driver-standigs-table-body">
+          <tr className="driver-standigs-table-row-label">
+            <td className="driver-standigs-table-cell">Position</td>
+            <td className="driver-standigs-table-cell">Name</td>
+            <td className="driver-standigs-table-cell">Points</td>
+            <td className="driver-standigs-table-cell">Wins</td>
+            <td className="driver-standigs-table-cell">Team</td>
           </tr>
-        );
-      });
-    return (
-      <div className="Standings">
-        <div className="Standings-label">2021 DRIVERS Standings</div>
-        <table className="driver-standigs-table">
-          <tbody className="driver-standigs-table-body">
-            <tr className="driver-standigs-table-row-label">
-              <td className="driver-standigs-table-cell">Position</td>
-              <td className="driver-standigs-table-cell">Name</td>
-              <td className="driver-standigs-table-cell">Points</td>
-              <td className="driver-standigs-table-cell">Wins</td>
-              <td className="driver-standigs-table-cell">Number</td>
-              <td className="driver-standigs-table-cell">Nationality</td>
-              <td className="driver-standigs-table-cell">Team</td>
-            </tr>
-            <StandingTable />
-          </tbody>
-        </table>
-        <div className="constructors-standings-page-content f1logo">
-          <img src={logo} className="logo-standings" />
-        </div>
+          <StandingTable />
+        </tbody>
+      </table>
+      <div className="constructors-standings-page-content f1logo">
+        <img src={logo} className="logo-standings" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default DriversStandingsPage;
